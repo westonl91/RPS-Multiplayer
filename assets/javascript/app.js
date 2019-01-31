f9ab8fdacc05ae4cd96042d6c522add558e412bb
Leah Weston <westonl91@yahoo.com>
2019-01-30 16:32:34 -0800

    initial push

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//Challenge HW for unit 7
+
+
+//possible twist to add: game is best out of three, but a player can only choose each option once.
+//if I get really bored. I could use the giphyAPI to play a 'rock beating scissors' and so on gif after every round
+
+// Create a variable to reference the database
+var database = firebase.database();
+
